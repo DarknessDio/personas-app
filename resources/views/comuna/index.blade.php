0c467a751f0ae8453e157ccd98d00e98d27a2c35
Dionny Velez <monin2244@gmail.com>
2024-03-30 17:39:09 -0500

    Commune Add

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -12,7 +12,7 @@
   <body>
     <div class="container">
       <h1>Listado de Municipios</h1>
-      <a href="" class="btn btn-success">Add</a>
+      <a href="{{route('comunas.create')}}" class="btn btn-success">Add</a>
         <table class="table">
             <thead>
                 <tr>
