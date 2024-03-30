6e4d4ba5504c24773e3c6e7b5de5d138a723a858
Dionny Velez <monin2244@gmail.com>
2024-03-30 17:47:46 -0500

    Commune Edit

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -7,11 +7,11 @@
  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Listado de Municipios</title>
+    <title>Listado de Comunas</title>
   </head>
   <body>
     <div class="container">
-      <h1>Listado de Municipios</h1>
+      <h1>Listado de Comunas</h1>
       <a href="{{route('comunas.create')}}" class="btn btn-success">Add</a>
         <table class="table">
             <thead>
@@ -29,6 +29,7 @@
                 <td>{{$comuna->comu_nomb}}</td>
                 <td>{{$comuna->muni_nomb}}</td>
                 <td>
+                <a href="{{route('comunas.edit', ['comuna'=> $comuna->comu_codi])}}" class="btn btn-info">Edit</a>
                 <form action="{{route('comunas.destroy' , ['comuna' => $comuna->comu_codi])}}"
                   method="POST" style="display: inline-block">
                   @method('delete')
